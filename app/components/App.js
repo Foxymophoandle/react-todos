e032c92e8fbe8ae4edcba3fa82cecfc1d3ef788e
Davide Favero <faverodavide86@gmail.com>
2017-06-26 13:57:00 +0200

    add and remove working

--- app/components/App.js
+++ app/components/App.js
@@ -4,15 +4,38 @@
 var React = require('react');
 
 
-function TodoItem(props) {
-    return (<div>{props.todo.text}</div>);
+class TodoItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleRemoveTodo = this.handleRemoveTodo.bind(this);
+    }
+
+    handleRemoveTodo(){
+        this.props.remove_todo(this.props.todo.id)
+    }
+
+    render (){
+        return (
+            <div>
+                {this.props.todo.text}
+                <input type="button" value="X" onClick={this.handleRemoveTodo} />
+            </div>
+        );
+    }
 }
 
 
 function TodoList(props){
 
     var todo_items = props.todos.map((todo) =>
-        <li key={todo.id}><TodoItem todo={todo}/></li>
+        <li key={todo.id}>
+            <TodoItem
+                todo={todo}
+                remove_todo={props.remove_todo}
+            />
+        </li>
     );
     return (
         <ul>{todo_items}</ul>
@@ -22,19 +45,31 @@ function TodoList(props){
 
 class AddTodo extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {todo_text: ''};
+
+        this.handleAddTodo = this.handleAddTodo.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({todo_text: event.target.value});
+    }
+
+    handleAddTodo(event) {
+        event.preventDefault();
+        this.props.add_todo(this.state.todo_text);
+        this.setState({todo_text: ''});
+    }
+
     render(){
-        let input;
 
         return (
-            <div>
-                 <form onSubmit={(e) => {
-                     e.preventDefault();
-                     this.props.add_todo(input.value);
-                     input.value = '';
-                 }}>
-                     <input ref={node => {input = node;}} />
-                 </form>
-            </div>
+            <form onSubmit={this.handleAddTodo}>
+                <input type="text" value={this.state.todo_text} onChange={this.handleChange} />
+                <input type="submit" value="Submit" />
+            </form>
         )
     }
 }
@@ -44,29 +79,31 @@ class TodoApp extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {todo_list: [
-            {'id': 1, 'text': 'first', 'done': false},
-            {'id': 2, 'text': 'second', 'done': false},
-            {'id': 3, 'text': 'third', 'done': true},
-        ]};
+        this.state = {
+            todo_list: [
+                {'id': 1, 'text': 'first', 'done': false},
+                {'id': 2, 'text': 'second', 'done': false},
+                {'id': 3, 'text': 'third', 'done': true}
+            ],
+            max_id: 3
+        };
 
         this.handleAddTodo = this.handleAddTodo.bind(this);
-        this.handleChangeTodoState = this.handleChangeTodoState.bind(this);
         this.handleRemoveTodo = this.handleRemoveTodo.bind(this);
     }
 
     handleAddTodo(todo){
-        var new_id = this.state.todo_list.length + 1;
+        var new_id = this.state.max_id + 1;
         this.state.todo_list.push({'id': new_id, 'text': todo, 'done': false});
-        this.setState({todo_list: this.state.todo_list})
-    }
-
-    handleChangeTodoState(todo_id){
-
+        this.setState({todo_list: this.state.todo_list});
+        this.setState({max_id: new_id})
     }
 
     handleRemoveTodo(todo_id){
-
+        var cleaned_todos = this.state.todo_list.filter((todo) => {
+            if (todo.id !== todo_id) return todo;
+        });
+        this.setState({todo_list: cleaned_todos})
     }
 
     render() {
@@ -78,7 +115,6 @@ class TodoApp extends React.Component {
                 <TodoList
                     todos={this.state.todo_list}
                     remove_todo={this.handleRemoveTodo}
-                    change_todo={this.handleChangeTodoState}
                 />
             </div>
         )
